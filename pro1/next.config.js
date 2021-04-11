6be4e78d056ce3bfd43d85eee822e144548b16b1
cyberghost002 <49005095+cyberghost002@users.noreply.github.com>
2021-04-12 00:33:44 +0530

    backend done

---
+++ pro1/next.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    env: {
+        // MONGO_URI: "mongodb+srv://KartikJaiswal:l13yFSWpb2YU1qR5@blog.smyeg.mongodb.net/NextNotes?retryWrites=true&w=majority"
+        MONGO_URI: "mongodb://127.0.0.1:27017/NotesNext"
+    }
+}
